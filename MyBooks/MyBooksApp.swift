8e5404f9bb0221e52c9d34326dde8f58090afa20
Rob Ranf <rob@emiyaconsulting.com>
2024-03-28 14:32:33 -0700

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Rob Ranf on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
